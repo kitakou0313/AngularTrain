58e88242386cf14e42cda2ab71933c43b721e989
kitakou0313 <kitakou0313@gmail.com>
2021-09-06 23:58:01 +0900

    feat: Add message display

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
+import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { Hero } from "../hero";
 import { HeroService } from "../hero.service";
+import { MessageService } from "../message.service";
 
 @Component({
   selector: 'app-heroes',
@@ -13,13 +15,14 @@ export class HeroesComponent implements OnInit {
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id = ${hero.id}`)
   }
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes)
   }
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
