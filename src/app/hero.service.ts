3a3daab543527ff1b394b55b34dbc7dfdf879a4e
kitakou0313 <kitakou0313@gmail.com>
2021-09-09 00:00:32 +0900

    Delete get method

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -19,12 +19,4 @@ export class HeroService {
     this.messageService.add("HeroService: fetched heroes")
     return heroes;
   }
-
-  getHero(id: number): Observable<Hero> {
-    const url = `${this.heroesUrl}/${id}`;
-    return this.http.get<Hero>(url).pipe(
-      tap(_ => this.log(`fetched hero id=${id}`)),
-      catchError(this.handleError<Hero>(`getHero id=${id}`))
-    );
-  }
 }
